4547a69ebb0be9843168cd85265f05759b0f9ab5
Karan-Fintech <karanacharya2003@gmail.com>
2025-09-20 03:13:06 +0530

    Done

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,27 +1,11 @@
-const request = require("supertest");
-const express = require("express");
-const authRouter = require("../routes/authRoutes");
+const express = require('express');
+const router = express.Router();
+// const { register, login } = require('../controllers/authController');
+const {register,login} = require('../controllers/authControllers')
 
-const app = express();
-app.use(express.json());
-app.use("/auth", authRouter);
 
-describe("Auth Routes", () => {
-  test("POST /auth/register should register a user", async () => {
-    const res = await request(app)
-      .post("/auth/register")
-      .send({ username: "testuser", password: "secret" });
+router.post('/register', register);
+router.post('/login', login);
 
-    expect(res.status).toBe(201); // expect 201 Created
-    expect(res.body).toHaveProperty("message", "User registered successfully");
-  });
 
-  test("POST /auth/login should login a user", async () => {
-    const res = await request(app)
-      .post("/auth/login")
-      .send({ username: "testuser", password: "secret" });
-
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty("token");
-  });
-});
+module.exports = router;
